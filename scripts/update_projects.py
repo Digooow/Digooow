e19492f2071dd242772a825d70fc5e21b6cf73d6
Rodrigo da Cruz Godinho <digo13045@gmail.com>
2026-06-21 15:57:56 -0300

    Simplify card generation in generate_cards function
    Refactor generate_cards function to simplify card generation and remove unused table HTML.

--- scripts/update_projects.py
+++ scripts/update_projects.py
@@ -25,36 +25,15 @@ def generate_cards(repos):
     if not repos:
         return "<!-- Nenhum projeto encontrado com o tópico 'showcase' -->"
 
-    cache_buster = int(time.time())
+    cache_buster = int(time.time())  # ÚNICA ADIÇÃO
 
+    cards = []
+    for repo in repos:
+        card_url = f"https://github-readme-stats.vercel.app/api/pin/?username={USERNAME}&repo={repo}&theme=dark&show_owner=true&description_lines_count=2&_={cache_buster}"
+        card_html = f'<a href="https://github.com/{USERNAME}/{repo}">\n  <img align="left" src="{card_url}" />\n</a>\n'
+        cards.append(card_html)
 
-    table_html = '<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse; width: 100%;">\n'
-
-    for i in range(0, len(repos), 2):
-        table_html += '  <tr>\n'
-
-        repo1 = repos[i]
-        card_url1 = f"https://github-readme-stats.vercel.app/api/pin/?username={USERNAME}&repo={repo1}&theme=dark&show_owner=true&description_lines_count=2&_={cache_buster}"
-        table_html += f'''    <td style="border: none; padding: 5px; width: 50%; vertical-align: top;">
-      <a href="https://github.com/{USERNAME}/{repo1}">
-        <img src="{card_url1}" style="max-width: 100%; height: auto; display: block;" />
-      </a>
-    </td>\n'''
-
-        if i+1 < len(repos):
-            repo2 = repos[i+1]
-            card_url2 = f"https://github-readme-stats.vercel.app/api/pin/?username={USERNAME}&repo={repo2}&theme=dark&show_owner=true&description_lines_count=2&_={cache_buster}"
-            table_html += f'''    <td style="border: none; padding: 5px; width: 50%; vertical-align: top;">
-      <a href="https://github.com/{USERNAME}/{repo2}">
-        <img src="{card_url2}" style="max-width: 100%; height: auto; display: block;" />
-      </a>
-    </td>\n'''
-        else:
-            table_html += '    <td style="border: none; padding: 5px; width: 50%;"></td>\n'
-        table_html += '  </tr>\n'
-
-    table_html += '</table>\n<div style="clear: both; margin-bottom: 30px;"></div>'
-    return table_html
+    return "\n".join(cards) + '\n\n<div style="clear: both; margin-bottom: 30px;"></div>'
 
 def update_readme(cards_html):
     with open(README_PATH, "r", encoding="utf-8") as f:
